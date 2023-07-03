35f80e505f1a4014f6411474b502cae2053ec75a
Pablo Diaz R <pablo.diaz.r85@gmail.com>
2023-07-03 11:31:51 -0400

    Se añade logo a Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,17 +1,23 @@
 import { Box, Typography } from '@mui/material';
+import styled from '@emotion/styled';
+import logo from '../assets/images/cryptologov2.png';
 
 const Hero = () => {
+  const Img = styled('img')({
+    width: 120,
+    height: 128,
+  });
   return (
     <Box
       display={'flex'}
       justifyContent={'center'}
       alignItems={'center'}
       flexDirection={'column'}
-      gap={1}
       pt={3}
       pb={3}
       textAlign={'center'}
     >
+      <Img src={logo} alt='logo' />
       <Typography variant='h2' fontWeight={'Bold'}>
         Coin look up!
       </Typography>
